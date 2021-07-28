e01b01ef720fb807e23b6229a99422ad15b68745
Oscar Medina <ogmedina04@gmail.com>
2021-07-28 11:35:13 -0500

    changed router to browserrouter from hashrouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import { HashRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./pages/Main";
 import NoMatch from "./pages/NoMatch";
 import Footer from './Components/Footer/footer';
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
     <div className="App">    
       <Switch>        
         <Route exact path="/" component={Main} />
@@ -15,7 +15,7 @@ function App() {
       </Switch>
       <Footer />
     </div>
-    </HashRouter>
+    </Router>
   );
 }
 

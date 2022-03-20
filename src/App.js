a180f05530702dd6b2dfb01a2a2064b744195730
Oscar Medina <ogmedina04@gmail.com>
2022-03-20 15:01:26 -0500

    commented out future code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./pages/Main";
-import Blog from "./pages/blog";
+// import Blog from "./pages/blog";
 import NoMatch from "./pages/NoMatch";
 import Footer from './Components/Footer/footer';
 
@@ -12,7 +12,7 @@ function App() {
       <Switch>        
         <Route exact path="/" component={Main} />
         <Route exact path="/main" component={Main} />
-        <Route exact path="/blog" component={Blog} />
+        {/* <Route exact path="/blog" component={Blog} /> */}
         <Route exact component={NoMatch} />    
       </Switch>
       <Footer />
